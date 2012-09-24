4a8fbd840da5ea71478b9c7f94452f4dd82b007f
Alex De Simone <ajdesimone@gmail.com>
2012-09-23 23:37:30 -0700

    Add untracked files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salesboard::Application.config.secret_token = '45d1134c46952d8093c76c3f21a2c541e5f5b04ed16d8d0510c0d7318711695ff886c23df9cd82e3d5a00a3205de5a82740fbe8202d8041e32a723dd74509acd'
